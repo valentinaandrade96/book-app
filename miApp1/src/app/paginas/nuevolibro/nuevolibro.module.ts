bc05c3801d324be3cecc7523c67355488fa348ae
valentina Andrade <andradevalentina96@gmail.com>
2023-12-03 21:42:54 +0100

    primerssssldkkklmk

--- miApp1/src/app/paginas/nuevolibro/nuevolibro.module.ts
+++ miApp1/src/app/paginas/nuevolibro/nuevolibro.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { NuevolibroPageRoutingModule } from './nuevolibro-routing.module';
 
 import { NuevolibroPage } from './nuevolibro.page';
+import { ComponentesModule } from 'src/app/componentes/componentes.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    NuevolibroPageRoutingModule
+    NuevolibroPageRoutingModule,
+    ComponentesModule,
+    ReactiveFormsModule
   ],
   declarations: [NuevolibroPage]
 })
--- miApp1/src/app/paginas/nuevolibro/nuevolibro.page.ts
+++ miApp1/src/app/paginas/nuevolibro/nuevolibro.page.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Articulo } from 'src/app/interfaces/interfaces';
 
 @Component({
   selector: 'app-nuevolibro',
@@ -6,10 +10,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nuevolibro.page.scss'],
 })
 export class NuevolibroPage implements OnInit {
-
-  constructor() { }
+ 
+  articuloForm: FormGroup;
+  constructor(private http: HttpClient, private formBuilder: FormBuilder, private router: Router,) {
+    
+  }
+ 
+  
 
   ngOnInit() {
+    this.articuloForm = this.formBuilder.group({
+      ISBN: ['', Validators.required],
+      titulo: ['', Validators.required],
+      precio_compra: [0, [Validators.required, Validators.min(0)]],
+      precio_venta: [0, [Validators.required, Validators.min(0)]],
+      categoria: ['', Validators.required],
+      descripcion: ['', Validators.required],
+      autor: ['', Validators.required],
+      proveedor: ['', Validators.required],
+      telefonoProveedor: ['', Validators.required],
+      img: ['', Validators.required],
+      stock: [0, [Validators.required, Validators.min(0)]]
+    });
   }
-
+  articulo:Articulo;
+  crearArticulo() {
+   
+    if (this.articuloForm.valid) {
+      this.articulo = {
+        ISBN: this.articuloForm.value.ISBN ,
+        titulo: this.articuloForm.value.titulo ,
+        precio_compra: this.articuloForm.value.precio_compra ,
+        precio_venta: this.articuloForm.value.precio_venta ,
+        categoria: this.articuloForm.value.categoria ,
+        descripcion: this.articuloForm.value.descripcion ,
+        autor: this.articuloForm.value.autor ,
+        proveedor: this.articuloForm.value.proveedor ,
+        telefonoProveedor: this.articuloForm.value.telefonoProveedor ,
+        img: this.articuloForm.value.img ,
+        stock: this.articuloForm.value.stock 
+      };
+    this.http.post("https://bookserver-6e5c8a077822.herokuapp.com/articulo/post", this.articulo).subscribe((data: any) => {
+      console.log(data)
+      
+      if(data.status == 'Ok'){
+        alert("Articulo correctamente registrado.");
+        this.router.navigateByUrl('/libros')
+      }
+    });
+  }else{
+    alert('Formulario no válido');
+  }
+  }
 }
